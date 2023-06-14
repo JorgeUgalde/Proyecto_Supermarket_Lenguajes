0db5ca4003c7a209cd8a7894266eb7356185e451
jorge <jorge@KOKI>
2023-06-14 11:04:53 -0600

    Creacion de BD

---
+++ SuperMarket/Data/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using SuperMarket.Models;
+
+namespace SuperMarket.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+    }
+}
--- SuperMarket/Models/Product.cs
+++ SuperMarket/Models/Product.cs
@@ -1,9 +1,38 @@
-﻿namespace SuperMarket.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SuperMarket.Models
 {
     public class Product
     {
+        [Key]
         public int Id { get; set; }
 
+        [Required]
+        public string BarCode { get; set; }
+
+        [Required]
+        public int Price { get; set; }
+
+        [Required]
+        public int InStock { get; set; }
+
+        [Required]
+        [Range(0,1)]
+        public int IsActive { get; set; }
+
+        
+        public string PictureUrl { get; set; }
+
+        [Required]
+        public string Unit { get; set; }
+
+        [Display(Name = "Category")]
+        public int CategoryId { get; set; }
+
+        [Required]
+        [ForeignKey("CategoryID")]
+        public Category Category { get; set; }
 
     }
 }

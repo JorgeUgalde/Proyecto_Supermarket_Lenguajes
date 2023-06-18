98ddc501c5284c93077376e78c6aff8a8127d24f
jorge_ugalde <jorge.ugaldegamboa@ucr.ac.cr>
2023-06-17 21:00:05 -0600

    feat
    resolviendo problemas

--- SuperMarket/Program.cs
+++ SuperMarket/Program.cs
@@ -1,7 +1,6 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using SuperMarket.Data;
-using Microsoft.EntityFrameworkCore;
 using SuperMarket.Repository.Interfaces;
 using SuperMarket.Repository;
 

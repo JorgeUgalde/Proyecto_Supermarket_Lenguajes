410920451e2c45f4593c93905a41740613288286
ED19 <edwin.molina@ucr.ac.cr>
2023-06-23 16:57:44 -0600

    Metodo post y get details

--- SuperMarket/Areas/Operator/Controllers/ListOrderController.cs
+++ SuperMarket/Areas/Operator/Controllers/ListOrderController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using SuperMarket.Models;
+using SuperMarket.Models.ViewModels;
 using SuperMarket.Repository.Interfaces;
 using SuperMarket.Utilities;
 using System.Data;
@@ -46,6 +47,52 @@ namespace SuperMarket.Areas.Operator.Controllers
             });
 
             return Json(new { data = formattedList });
+        }
+
+        [HttpGet]
+        public IActionResult Details(int? id)
+        {
+            if (id != null)
+            {
+                if (id <= 0)
+                {
+                    return NotFound();
+                }
+                Order? OrderFromDB = _unitOfWork.Order.Get(x => x.Id == id);
+                OrderVM? OrderVMFromDB = new()
+                {
+                    Order = OrderFromDB,
+                    //ProductOrder = _unitOfWork.ProductRepository.GetAll().Select(i => 
+                    //IEnumerable)
+                };
+
+                if (OrderFromDB == null)
+                {
+                    return NotFound();
+                }
+                return View(OrderFromDB);
+            }
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Details(Category _category)
+        {
+            if (ModelState.IsValid)
+            {
+                if (_category.Id == 0 || _category.Id == null)
+                    _unitOfWork.Category.Add(_category);
+                else
+                    _unitOfWork.Category.Update(_category);
+
+                _unitOfWork.Save();
+                TempData["success"] = "Category saved successfully";
+            }
+            else
+            {
+                TempData["error"] = "Error creating category";
+            }
+            return RedirectToAction("Index");
         }
 
         //[HttpGet]
---
+++ SuperMarket/Models/ViewModels/OrderVM.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace SuperMarket.Models.ViewModels
+{
+    public class OrderVM
+    {
+        [ValidateNever]
+        public Order Order { get; set; }
+
+        [ValidateNever]
+        public IEnumerable<ProductOrder> ProductOrder { get; set; }
+        
+        
+        [ValidateNever]
+        public IEnumerable<Product> SelectedProducts { get; set; }
+
+       
+    }
+}

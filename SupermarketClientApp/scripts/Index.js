905dbd232d500408a9301dbc802a2b214b581040
jorge_ugalde <jorge.ugaldegamboa@ucr.ac.cr>
2023-07-01 10:57:42 -0600

    Pagina detalles, add to cart en progreso

--- SupermarketClientApp/scripts/Index.js
+++ SupermarketClientApp/scripts/Index.js
@@ -23,6 +23,43 @@ function fetchJsonData() {
     //     });
     // }
 
+    // function generateCards(data) {
+    //     let cardsContainer = $('#cardsContainer');
+    //     cardsContainer.empty();
+    //     let row = $('<div class="row"></div>');
+    
+    //     data.data.forEach(item => {
+    //         let listItem = $('<div class="card bg-dark text-white mb-3" style="max-width: 18rem;"></div>');
+    //         listItem.addClass('mb-3'); // Add margin class
+            
+    //         let image = $('<img src="' + item.pictureUrl + '" class="card-img-top" alt="">');
+    //         let cardBody = $('<div class="card-body"></div>');
+    //         let cardTitle = $('<h5 class="card-title">' + item.name + '</h5>');
+    //         let cardText = $('<p class="card-text">fuap</p>');
+    //         // let viewMoreLink = $('<a href="#" class="btn btn-primary">View more</a>');
+    //         let viewMoreLink = $('<a href="../pages/Details.html?id=' + item.id + '" class="btn btn-primary">View more</a>');
+
+    //         // Responsive classes for mobile and larger screens
+    //         image.addClass('img-mobile'); // Mobile: full width
+    //         cardTitle.addClass('card-title-mobile'); // Mobile: below the image
+    
+    //         // Append elements to the card body
+    //         cardBody.append(image);
+    //         cardBody.append(cardTitle);
+    //         cardBody.append(cardText);
+    //         cardBody.append(viewMoreLink);
+            
+    //         listItem.append(cardBody);
+    
+    //         row.append(listItem);
+    //     });
+    
+    //     cardsContainer.append(row);
+    // }
+
+
+
+
     function generateCards(data) {
         let cardsContainer = $('#cardsContainer');
         cardsContainer.empty();
@@ -31,12 +68,17 @@ function fetchJsonData() {
         data.data.forEach(item => {
             let listItem = $('<div class="card bg-dark text-white mb-3" style="max-width: 18rem;"></div>');
             listItem.addClass('mb-3'); // Add margin class
-            
+    
             let image = $('<img src="' + item.pictureUrl + '" class="card-img-top" alt="">');
             let cardBody = $('<div class="card-body"></div>');
             let cardTitle = $('<h5 class="card-title">' + item.name + '</h5>');
             let cardText = $('<p class="card-text">fuap</p>');
-            let viewMoreLink = $('<a href="#" class="btn btn-primary">View more</a>');
+            let viewMoreLink = $('<a href="../pages/Details.html?id=' + item.id + '" class="btn btn-primary">View more</a>');
+    
+            // Store product data in local storage when the user clicks on the "View more" button
+            viewMoreLink.on('click', function() {
+                localStorage.setItem('productData', JSON.stringify(item));
+            });
     
             // Responsive classes for mobile and larger screens
             image.addClass('img-mobile'); // Mobile: full width
@@ -47,7 +89,7 @@ function fetchJsonData() {
             cardBody.append(cardTitle);
             cardBody.append(cardText);
             cardBody.append(viewMoreLink);
-            
+    
             listItem.append(cardBody);
     
             row.append(listItem);
@@ -55,7 +97,31 @@ function fetchJsonData() {
     
         cardsContainer.append(row);
     }
+    
+
+    function filterDataByProductName(data, searchTerm) {
+        return data.data.filter(function(item) {
+            // Filtra los datos según el término de búsqueda (ignorando mayúsculas y minúsculas)
+            return item.name.toLowerCase().includes(searchTerm.toLowerCase());
+        });
+    }
+    
 
     $( document ).ready(function() {
+        $('#searchBar').on('input', function() {
+            var filterValue = $(this).val().toLowerCase();
+    
+            // Ocultar todas las tarjetas
+            $('.card').hide();
+    
+            // Mostrar solo las tarjetas que coinciden con el filtro
+            $('.card').each(function() {
+                var productName = $(this).find('.card-title').text().toLowerCase();
+    
+                if (productName.includes(filterValue)) {
+                    $(this).show();
+                }
+            });
+        });
         fetchJsonData();
     });
